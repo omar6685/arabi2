c695f946fd5a4f0ef73b0e65ee86cd65e712f1a7
omar6685 <omr6685@hotmail.com>
2022-08-06 18:56:40 +0300

    alot of chenges

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'testu1', email: 'testu1@gmail.com', password: 'testu1', password_confirmation: 'testu1')
+User.create(username: 'testu2', email: 'testu2@gmail.com', password: 'testu2', password_confirmation: 'testu2')
+User.create(username: 'testu3', email: 'testu3@gmail.com', password: 'testu3', password_confirmation: 'testu3')
+User.create(username: 'testu4', email: 'testu4@gmail.com', password: 'testu4', password_confirmation: 'testu4')
+User.create(username: 'testu5', email: 'testu5@gmail.com', password: 'testu5', password_confirmation: 'testu5')
+
+puts 'Users created'
